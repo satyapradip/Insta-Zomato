fbb326ea71be18b99eef2d8efa1c181700e2e129
Satyapradip <satyapradip7602@gmail.com>
2026-02-24 01:46:56 +0530

    feat: setup Express server with middleware, cookie-parser, and route mounting

---
+++ backend/src/app.js
@@ -0,0 +1,21 @@
+// create server
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/auth.routes");
+
+// create express app
+const app = express();
+
+// middleware to parse JSON, this will allow us to access req.body, which is where the data sent in the request will be stored
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// use auth routes
+app.use("/api/auth", authRoutes);
+
+// export the app module
+module.exports = app;
